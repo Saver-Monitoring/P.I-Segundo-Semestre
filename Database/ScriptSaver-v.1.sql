f12edf68bb48a20007214b192336d2f7cb18e96a
MBisacchi <mayla.bisacchi@sptech.school>
2022-10-04 11:47:49 -0300

    Script banco de dados Saver, primeira versão

---
+++ Database/ScriptSaver-v.1.sql
@@ -0,0 +1,97 @@
+-- Script: Projeto grupo 4 P&I 
+-- Saver
+
+-- Criação do banco de dados:
+create database saver;
+
+-- Excluir banco de dados:
+-- drop database saver;
+
+-- Usar banco de dados:
+use saver;
+
+-- Criação da tabela 'Empresa':
+create table empresa (
+idEmpresa int primary key auto_increment,
+nome varchar (45) not null,
+cnpj char (14) not null,
+email varchar(50) not null,
+cep char (8),
+rua varchar (50),
+numero int
+);
+
+-- Visualizar dados da tabela
+select * from empresa;
+
+-- Criação da tabela 'Usuário':
+create table usuario (
+idUsuario int primary key auto_increment,
+nomeUsuario varchar (20) not null,
+email varchar (60) not null,
+senha varchar (45) not null,
+fkEmpresa int,
+fkAdministrador int,
+foreign key (fkEmpresa) references empresa(idEmpresa),
+foreign key (fkAdministrador) references usuario(idUsuario)
+);
+
+-- Criação da tabela 'Rack':
+create table rack (
+idRack int primary key auto_increment,
+nome varchar (45),
+fkEmpresa int,
+foreign key (fkEmpresa) references empresa(idEmpresa)
+);
+
+-- Criação da tabela 'Acesso':
+create table acesso (
+fkUsuario int,
+fkEmpresa int,
+fkRack int,
+dataHora datetime default current_timestamp,
+foreign key (fkUsuario) references usuario(idUsuario),
+foreign key (fkEmpresa) references empresa(idEmpresa),
+foreign key (fkRack) references rack(idRack)
+);
+
+-- Criação da tabela 'Computador':
+create table computador (
+idComputador int primary key auto_increment,
+fkRack int,
+sistOperacional varchar (45),
+limTemp double,
+processador varchar (45), 
+qtdRAM int,
+qtdHDD int,
+qtdVRAM int,
+foreign key (fkRack) references rack(idRack)
+);
+
+-- Criação da tabela 'Histórico dados':
+create table historicoDados (
+idDados int primary key auto_increment,
+fkComputador int,
+fkRack int,
+temperatura double,
+usoRAM double,
+usoVRAM double,
+usoHDD double,
+dataHora datetime default current_timestamp,
+foreign key (fkComputador) references computador(idComputador),
+foreign key (fkRack) references rack(idRack)
+);
+
+-- Criação da tabela 'Limites':
+create table limite (
+idLimite int primary key auto_increment,
+limRAM double,
+limHDD double,
+limCPU double,
+limVRAM double,
+fkComputador int,
+fkRack int,
+foreign key (fkComputador) references computador(idComputador),
+foreign key (fkRack) references rack(idRack)
+);
+
